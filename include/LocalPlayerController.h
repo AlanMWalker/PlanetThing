6a790e2b2424c042bd96c1880f2b342c6cbecd7d
AlanMWalker <masimba@blueyonder.co.uk>
2020-06-15 14:19:25 +0100

    Created LocalPlayerController class

---
+++ include/LocalPlayerController.h
@@ -0,0 +1,15 @@
+#include <KComponent.h>
+
+class LocalPlayerController :
+	public Krawler::KComponentBase
+{
+public:
+
+	LocalPlayerController(Krawler::KEntity* pEntity);
+	~LocalPlayerController() = default;
+
+	
+private: 
+
+
+};
---
+++ src/LocalPlayerController.cpp
@@ -0,0 +1,6 @@
+#include "LocalPlayerController.h"
+
+LocalPlayerController::LocalPlayerController(Krawler::KEntity* pEntity)
+	: KComponentBase(pEntity)
+{
+}
