f202b088fb8a14b8cd05a799484e96edb64d94fd
A. Masimba Walker <AlanMWalker@users.noreply.github.com>
2020-06-15 14:20:01 +0100

    Added setup project bat (#20)

---
+++ setup_project.bat
@@ -0,0 +1,5 @@
+mkdir src
+cd src
+git clone https://github.com/Britbongs/ProjectMountainLobster.git
+git clone https://github.com/AlanMWalker/KRAWLER.git
+git clone https://github.com/Britbongs/ThaServa.git
